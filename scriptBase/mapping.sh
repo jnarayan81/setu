03227952a3db6c2779eeea39a7fd98d334e048ee
Nityendra Shukla <nitinshukla218@gmail.com>
2023-02-10 14:50:40 +0530

    Update mapping.sh
    Signed-off-by: Nityendra Shukla <nitinshukla218@gmail.com>

--- scriptBase/mapping.sh
+++ scriptBase/mapping.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #These files could be use to visualie it in 'tablet' or 'IGV'
-  bwa index ragout_spades/setu_scaffolds.filled.fa
+  mkdir -p visualization
+  bwa index ragout/setu_scaffolds.fasta
   #bwa mem ragout_spades/setu_scaffolds.filled.fa sequence_PR1_trimmed.fq sequence_PR2_trimmed.fq > finalAln-pe.sam
-  bwa mem ragout_spades/setu_scaffolds.filled.fa lib_JIT_mapped.1.fastq lib_JIT_mapped.2.fastq > finalAln-pe.sam
-  samtools sort finalAln-pe.sam > finalAln-pe.sorted.bam
-  samtools index finalAln-pe.sorted.bam
-
+  bwa mem ragout/setu_scaffolds.fasta lib_JIT_mapped.1.fastq lib_JIT_mapped.2.fastq > visualization/finalAln-pe.sam
+  samtools sort visualization/finalAln-pe.sam > visualization/finalAln-pe.sorted.bam
+  samtools index visualization/finalAln-pe.sorted.bam
 
